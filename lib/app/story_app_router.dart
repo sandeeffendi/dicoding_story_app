a68e0bcbe4b68aa9242763d668544d8034ca2e32
sandeeffendi <sande.effendi@gmail.com>
2025-11-26 21:47:13 +0700

    feat(auth): initiate story app router

---
+++ lib/app/story_app_router.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:intermediate_first_submission/presentation/pages/auth/login/login_page.dart';
+import 'package:intermediate_first_submission/presentation/pages/auth/register/register_page.dart';
+import 'package:intermediate_first_submission/presentation/pages/auth/splash_page.dart';
+
+class StoryAppRouter {
+  static const String splash = '/splash';
+  static const String login = '/login';
+  static const String register = 'register';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      // splash route
+      case splash:
+        return MaterialPageRoute(
+          builder: (_) => SplashPage(),
+          settings: settings,
+        );
+
+      // login route
+      case login:
+        return MaterialPageRoute(builder: (_) => LoginPage());
+
+      // register route
+      case register:
+        return MaterialPageRoute(builder: (_) => RegisterPage());
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) =>
+              Scaffold(body: Center(child: Text('404 - Page not Found'))),
+        );
+    }
+  }
+}
