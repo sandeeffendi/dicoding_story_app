d5b68f670116a4ebef0a7955dfc5043c00156261
sandeeffendi <sande.effendi@gmail.com>
2025-11-27 20:25:50 +0700

    feat(auth): create auth repository impl on data layer

--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -1,16 +1,35 @@
+import 'package:intermediate_first_submission/data/datasource/main_remote_datasource.dart';
 import 'package:intermediate_first_submission/domain/enitities/login_result_entity.dart';
 import 'package:intermediate_first_submission/domain/enitities/register_response_entity.dart';
 import 'package:intermediate_first_submission/domain/repositories/auth_repository.dart';
 
-class AuthRepositoryImpl extends AuthRepository{
+class AuthRepositoryImpl implements AuthRepository {
+  final MainRemoteDatasource remoteDatasource;
+  const AuthRepositoryImpl(this.remoteDatasource);
+
   @override
-  Future<RegisterResponseEntity> createAccount({required String email, required String password, required String name}) {
-    // TODO: implement createAccount
-    throw UnimplementedError();
+  Future<RegisterResponseEntity> createAccount({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    final response = await remoteDatasource.createAccount(
+      name: name,
+      email: email,
+      password: password,
+    );
+
+    return RegisterResponseEntity(
+      error: response.error,
+      message: response.message,
+    );
   }
 
   @override
-  Future<LoginResultEntity> logIn({required String email, required String password}) {
+  Future<LoginResultEntity> logIn({
+    required String email,
+    required String password,
+  }) {
     // TODO: implement logIn
     throw UnimplementedError();
   }
