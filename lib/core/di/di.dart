15efd3cb954c1abdc0850e1399efe8b1519e5270
sandeeffendi <sande.effendi@gmail.com>
2025-11-28 20:35:28 +0700

    feat(auth): adding service to handle session with shared preferences

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -3,6 +3,7 @@ import 'package:intermediate_first_submission/data/datasource/main_remote_dataso
 import 'package:intermediate_first_submission/data/repositories/auth_repository_impl.dart';
 import 'package:intermediate_first_submission/domain/repositories/auth_repository.dart';
 import 'package:intermediate_first_submission/domain/usecases/create_account_usecase.dart';
+import 'package:intermediate_first_submission/domain/usecases/login_usecase.dart';
 import 'package:intermediate_first_submission/env/env.dart';
 import 'package:intermediate_first_submission/presentation/pages/auth/provider/auth_provider.dart';
 
@@ -11,13 +12,19 @@ final sl = GetIt.instance;
 Future<void> init() async {
   final baseUrl = Env.dicodingApiEndpoint;
 
-  sl.registerFactory(() => AuthProvider(createAccountUsecase: sl()));
-
-  sl.registerLazySingleton(() => CreateAccountUsecase(sl()));
+  sl.registerFactory(
+    () => AuthProvider(createAccountUsecase: sl(), loginUsecase: sl()),
+  );
 
   sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(sl()));
 
   sl.registerLazySingleton<MainRemoteDatasource>(
     () => MainRemoteDatasource(baseUrl: baseUrl),
   );
+
+  // create account config
+  sl.registerLazySingleton(() => CreateAccountUsecase(sl()));
+
+  // login config
+  sl.registerLazySingleton(() => LoginUsecase(sl()));
 }
--- lib/core/services/session_services.dart
+++ lib/core/services/session_services.dart
@@ -1,1 +1,31 @@
-class SessionServices {}
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SessionServices {
+  static const String _accessTokenKey = 'access_token';
+
+  final SharedPreferences _prefs;
+  SessionServices(this._prefs);
+
+  // save token method
+  Future<void> saveToken({required String accessToken}) async {
+    await _prefs.setString(_accessTokenKey, accessToken);
+  }
+
+  // get token method
+  String? getAccessToken() {
+    return _prefs.getString(_accessTokenKey);
+  }
+
+  // check session method
+  bool isLoggedIn() {
+    final token = getAccessToken();
+    if (token == null || token.isEmpty) return false;
+
+    return true;
+  }
+
+  // clear session method
+  Future<void> clearSession() async {
+    await _prefs.remove(_accessTokenKey);
+  }
+}
