86c56f1500038dd3181aa45ac86474f0bd5d5b67
sandeeffendi <sande.effendi@gmail.com>
2025-12-18 00:22:24 +0700

    feat(detail): create detail maps page and implement google maps for location service

---
+++ lib/features/detail/pages/detail_maps_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intermediate_first_submission/generated/l10n/app_localizations.dart';
+
+class DetailMapsPage extends StatefulWidget {
+  const DetailMapsPage({super.key});
+
+  @override
+  State<DetailMapsPage> createState() => _DetailMapsPageState();
+}
+
+class _DetailMapsPageState extends State<DetailMapsPage> {
+  late GoogleMapController mapController;
+
+  final Set<Marker> markers = {};
+
+  final dicodingOffice = LatLng(-6.8957473, 107.6337669);
+
+  final MapType mapType = MapType.normal;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final marker = Marker(
+      markerId: const MarkerId('Dicoding Office'),
+      position: dicodingOffice,
+      onTap: () {
+        mapController.animateCamera(CameraUpdate.newLatLng(dicodingOffice));
+      },
+    );
+
+    markers.add(marker);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(AppLocalizations.of(context)!.locationTitle)),
+      body: Center(
+        child: Stack(
+          children: [
+            GoogleMap(
+              mapType: mapType,
+              markers: markers,
+              initialCameraPosition: CameraPosition(
+                target: dicodingOffice,
+                zoom: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/generated/l10n/app_localizations_en.dart
+++ lib/generated/l10n/app_localizations_en.dart
@@ -76,4 +76,7 @@ class AppLocalizationsEn extends AppLocalizations {
 
   @override
   String get othersTitle => 'Others';
+
+  @override
+  String get locationTitle => 'Location';
 }
