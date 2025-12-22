3164ca58c3664b4b96c0b75b5457c58cfdeec344
sandeeffendi <sande.effendi@gmail.com>
2025-12-22 21:18:01 +0700

    feat(detial): show locaiton info from current detial story location

--- lib/features/detail/pages/detail_maps_page.dart
+++ lib/features/detail/pages/detail_maps_page.dart
@@ -27,19 +27,61 @@ class _DetailMapsPageState extends State<DetailMapsPage> {
 
   final MapType mapType = MapType.normal;
 
+  geo.Placemark? placemark;
+
   @override
   void initState() {
     super.initState();
 
     final currentPositiion = LatLng(widget.storyLat, widget.storyLon);
 
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final LatLng latLng = LatLng(widget.storyLat, widget.storyLon);
+
+      final info = await geo.placemarkFromCoordinates(
+        widget.storyLat,
+        widget.storyLon,
+      );
+
+      final place = info[0];
+      final street = place.street;
+      final String address =
+          '${place.subLocality}, ${place.locality},${place.postalCode},${place.country}';
+
+      setState(() {
+        placemark = place;
+      });
+
+      defineMarker(latLng, street, address);
+    });
+
     final marker = Marker(
-      markerId: const MarkerId('Dicoding Office'),
+      markerId: const MarkerId('source'),
       position: currentPositiion,
     );
 
     markers.add(marker);
   }
+
+  // define marker
+  void defineMarker(LatLng latLng, String? street, String address) {
+    final Marker marker = Marker(
+      markerId: const MarkerId('source'),
+      position: latLng,
+      infoWindow: InfoWindow(
+        title: street ?? 'Unnamed Street',
+        snippet: address,
+      ),
+    );
+
+    setState(() {
+      markers.clear();
+      markers.add(marker);
+    });
+  }
+
+  // onCurentLocationMarkerPress
+  void onCurrentLocationPress() {}
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +106,7 @@ class _DetailMapsPageState extends State<DetailMapsPage> {
                 target: storyCurrentPosition,
                 zoom: 18,
               ),
-              onMapCreated: (controller) {
+              onMapCreated: (controller) async {
                 setState(() {
                   mapController = controller;
                 });
@@ -77,3 +119,45 @@ class _DetailMapsPageState extends State<DetailMapsPage> {
   }
 }
 
+class PlacemarkWidget extends StatelessWidget {
+  const PlacemarkWidget({super.key, required this.placemark});
+  final geo.Placemark placemark;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      constraints: const BoxConstraints(maxWidth: 700),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.all(Radius.circular(50)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            blurRadius: 20,
+            offset: Offset.zero,
+            color: Colors.grey.withAlpha((0.5 * 255).round()),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  placemark.street!,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Text(
+                  '${placemark.subLocality}, ${placemark.locality}, ${placemark.postalCode}, ${placemark.country}',
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
